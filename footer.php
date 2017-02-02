1fbc596272e1f3c862cd932a8dc0af9551e18c77
Phil Kerrigan <phil.kerrigan@gmail.com>
2017-02-01 22:29:56 -0500

    poop

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores' ), 'underscores', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
-
+poop
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
